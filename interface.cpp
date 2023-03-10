e5e0f1108ed4c0e7f478f9c244c2ce8964790814
Geovanny Garcia Downing <geogd.712@estudiantec.cr>
2023-03-10 15:58:25 -0600

    Buttons conected with IDs

--- interface.cpp
+++ interface.cpp
@@ -4,6 +4,11 @@
     #include <wx/wx.h>
 #endif
 
+#include <iostream>
+
+using namespace std;
+
+
 class MyApp : public wxApp
 {
 public:
@@ -16,10 +21,33 @@ class MyFrame : public wxFrame
 public:
     MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
     wxBoxSizer* m_mainSizer;
+    std::vector<std::string> buttonLabels = {
+    "Button 1",
+    "Button 2",
+    "Button 3",
+    "Button 4",
+    "Button 5",
+    "Button 6",
+    "Button 7",
+    "Button 8",
+    "Button 9",
+    "Button 10",
+    "Button 11",
+    "Button 12",
+    "Button 13",
+    "Button 14",
+    "Button 15",
+    "Button 16",
+    "Button 17",
+    "Button 18",
+    "Button 19",
+    "Button 20",
+    "Button 21"
+};
 
 private:
     
-    void OnClick(wxCommandEvent& event);
+    void OnButtonClicked(wxCommandEvent& event);
 
 };
 
@@ -42,11 +70,22 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
 
     wxGridSizer* buttonSizer = new wxGridSizer(7, 3, 10, 10);
 
-    for (int i = 0; i < 21; i++)
+    int i = 1;
+    int j = 1;
+
+    for (const auto& label : buttonLabels)
     {
-        wxString buttonText = wxString::Format(wxT("Button %d"), i + 1);
-        wxButton* button = new wxButton(this, wxID_ANY, buttonText, wxDefaultPosition, wxSize(100, 50));
+        wxButton* button = new wxButton(this, i*10+j, wxString::FromUTF8(label), wxDefaultPosition, wxSize(100, 50));
         buttonSizer->Add(button, 0, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL);
+        Connect(i*10+j, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnButtonClicked));
+        if (j == 3)
+        {
+            i++;
+            j=1;
+        } else
+        {
+            j++;
+        }  
     }
 
    m_mainSizer->Add(buttonSizer, 0, wxALIGN_CENTER_HORIZONTAL|wxTOP, 200);;
@@ -54,7 +93,7 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     Layout();
 }
 
-void MyFrame::OnClick(wxCommandEvent& event)
+void MyFrame::OnButtonClicked(wxCommandEvent& event)
 {
-    
+    cout<<event.GetId()<<endl;
 }
